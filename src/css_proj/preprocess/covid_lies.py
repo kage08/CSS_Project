3fbb2eb8f43f14b836d2153d2bd3933d4a8245cb
kage08 <harshavardhan864.hk@gmail.com>
2022-04-14 12:15:17 -0400

    Stateful extraction for Covid-lies

--- src/css_proj/preprocess/covid_lies.py
+++ src/css_proj/preprocess/covid_lies.py
@@ -7,6 +7,7 @@ from .consts import (
 import tweepy
 from tqdm import tqdm
 import logging
+import pickle
 
 
 cvl_features = [
@@ -32,6 +33,72 @@ cvl_features = [
     "label",
 ]
 
+INTERMEDIATE_PATH = "./data/data_processed/lies_data_intermediate.pkl"
+
+
+def get_empty_df_lies(path: str = COVID_LIES_PATH, save: bool = True):
+    """
+    Get an empty dataset for the COVID-19 cure dataset.
+
+    ---
+    Parameters:
+    ---
+    path (str): The path to the COVID-19 lies dataset (csv).
+    """
+    if os.path.exists(INTERMEDIATE_PATH):
+        df = load_intermediate_df_lies()
+        return df
+
+    df = pd.read_csv(path)
+    df["state"] = 0
+    df["tweet_text"] = ""
+    df["retweets"] = 0
+    df["likes"] = 0
+    df["created"] = ""
+    df["lang"] = ""
+    df["device"] = ""
+    df["user_id"] = 0
+    df["user_name"] = ""
+    df["user_handle"] = ""
+    df["user_location"] = ""
+    df["user_description"] = ""
+    df["user_following"] = 0
+    df["user_followers"] = 0
+    df["user_verified"] = 0
+    df["user_total_tweets"] = 0
+    df["user_total_likes"] = 0
+    df["user_total_lists"] = 0
+    df["misconception_id"] = ""
+    if save:
+        save_intermediate_df_lies(df)
+    return df
+
+
+def save_intermediate_df_lies(df, path: str = INTERMEDIATE_PATH):
+    """
+    Save the intermediate dataset to the given path.
+
+    ---
+    Parameters:
+    ---
+    path (str): The path to the intermediate dataset.
+    """
+    with open(path, "wb") as f:
+        pickle.dump(df, f)
+
+
+def load_intermediate_df_lies(path: str = INTERMEDIATE_PATH):
+    """
+    Load the intermediate dataset from the given path.
+
+    ---
+    Parameters:
+    ---
+    path (str): The path to the intermediate dataset.
+    """
+    with open(path, "rb") as f:
+        return pickle.load(f)
+
 
 def download_covid_lies(url: str = COVID_LIES_URL, path: str = COVID_LIES_PATH):
     """
@@ -50,6 +117,37 @@ def download_covid_lies(url: str = COVID_LIES_URL, path: str = COVID_LIES_PATH):
     with open(path, "wb") as f:
         f.write(r.content)
     print("Done.")
+
+
+def get_tweet_info(df: pd.DataFrame, idx: int, api):
+    """
+    Get the tweet info of the tweet with the given index.
+
+    ---
+    Parameters:
+    ---
+    idx (int): The index of the tweet.
+    """
+
+    tweet_id = df.iloc[idx]["tweet_id"]
+    if df.iloc[idx]["state"] != 0:
+        return df
+    try:
+        feat_dict = get_tweet_features(tweet_id, api)
+        for feat in feat_dict:
+            df.iloc[idx, df.columns.get_loc(feat)] = feat_dict[feat]
+        df.iloc[idx, df.columns.get_loc("state")] = 1
+        save_intermediate_df_lies(df)
+        return df
+    except Exception as e:
+        if "Forbidden" in str(e):
+            df.iloc[idx, df.columns.get_loc("state")] = -1
+            save_intermediate_df_lies(df)
+            print(e)
+            return df
+        print(f"Failed to get tweet {tweet_id} at index {idx}")
+        print(e)
+        return e
 
 
 def get_tweet_features(tweet_id: str, api) -> dict:
@@ -85,29 +183,44 @@ def get_tweet_features(tweet_id: str, api) -> dict:
     return entry
 
 
-def get_df_covid_lies(api, path: str = COVID_LIES_PATH, start_idx: int = 0):
+def get_df_lies(
+    api, path: str = COVID_LIES_PATH, start_idx: int = 0, end_idx: int = -1
+):
     """
-    Get the COVID-19 lies dataset from the given path.
+    Get the COVID-19 cure dataset from the given path.
 
     ---
     Parameters:
     ---
     path (str): The path to the COVID-19 lies dataset (csv).
     """
-    dataset = pd.DataFrame(columns=cvl_features)
-    df = pd.read_csv(path)
-    # Log to file
-    logging.basicConfig(filename="logs/covid_lies_extract.log", level=logging.INFO)
+    df = get_empty_df_lies(path, save=True)
+    if end_idx == -1:
+        end_idx = len(df)
+    for idx in tqdm(range(start_idx, end_idx)):
+        df = get_tweet_info(df, idx, api)
+        if type(df) != pd.DataFrame:
+            print("Rate limit exceeded at index {}".format(idx))
+            print("Try again later")
+            break
+    return df
 
-    for i in tqdm(range(start_idx, len(df))):
-        try:
-            entry = get_tweet_features(str(df.iloc[i]["tweet_id"]), api)
-            entry["misconception_id"] = df.iloc[i]["misconception_id"]
-            entry["label"] = df.iloc[i]["label"]
-            dataset = pd.concat([dataset, pd.DataFrame([entry])], ignore_index=True)
-            logging.info(f"Extracted tweet {df.iloc[i]['tweet_id']}")
-        except Exception as e:
-            logging.warning(f"Failed to get tweet {df.iloc[i]['tweet_id']}")
-            logging.warning(e)
-            continue
-    return dataset
+
+def clean_up_intermediate(
+    path: str = INTERMEDIATE_PATH,
+    save_path="./data/data_processed/lies_data_cleaned.pkl",
+):
+    """
+    Clean up the intermediate dataset.
+
+    ---
+    Parameters:
+    ---
+    path (str): The path to the intermediate dataset.
+    """
+    df = load_intermediate_df_lies(path)
+    df = df[df["state"] == 1]
+    df = df.drop(columns=["state"])
+    with open(save_path, "wb") as f:
+        pickle.dump(df, f)
+    return df
